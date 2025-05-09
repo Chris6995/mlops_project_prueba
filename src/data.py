f2930de7f2765327d04e8775cd6d9d6884e9b045
Chris6995 <c.donaire95@gmail.com>
2025-05-09 11:01:31 +0200

    some changes in pipelines and new training pipeline created false with another model

--- src/data.py
+++ src/data.py
@@ -5,8 +5,9 @@ from typing import Tuple
 from typing import Optional, List
 from paths import RAW_DATA_DIR, TRANSFORMED_DATA_DIR
 from pathlib import Path
-from datetime import datetime
+from datetime import datetime, timedelta
 from dateutil.relativedelta import relativedelta
+import numpy as np
 
 # ---------------------------------------------------
 # Descargar datos
@@ -372,3 +373,109 @@ def load_last_12_months_data(end_date: datetime, months_ago: int = 12) -> pd.Dat
 
     rides_all = rides_all.sort_values("pickup_datetime").reset_index(drop=True)
     return rides_all
+
+def fetch_batch_raw_data(from_date: datetime, to_date: datetime) -> pd.DataFrame:
+    """
+    Simulate production data by sampling historical data from 52 weeks ago (i.e. 1 year)
+    """
+    from_date_ = from_date - timedelta(days=7*52) # 52 semanas anteriores
+    to_date_ = to_date - timedelta(days=7*52)
+    print(f'{from_date=}, {to_date_=}') 
+
+    # Descargamos los 2 archivos del sitio web
+    rides = load_raw_data_v2(year=from_date_.year, months=from_date_.month)
+    rides = rides[rides.pickup_datetime >= from_date_]
+    rides_2 = load_raw_data_v2(year=to_date_.year, months=to_date_.month)
+    rides_2 = rides_2[rides_2.pickup_datetime < to_date_]
+
+    rides = pd.concat([rides, rides_2])
+
+    # Cambiamos los datos para fingir que son datos recientes
+    rides['pickup_datetime'] += timedelta(days=7*52)
+
+    rides.sort_values(by=['pickup_location_id', 'pickup_datetime'], inplace=True)
+
+    return rides
+
+def get_cutoff_indices_features_and_target(
+    data: pd.DataFrame,
+    input_seq_len: int,
+    step_size: int
+    ) -> list:
+
+        stop_position = len(data) - 1
+        
+        # Start the first sub-sequence at index position 0
+        subseq_first_idx = 0
+        subseq_mid_idx = input_seq_len
+        subseq_last_idx = input_seq_len + 1
+        indices = []
+        
+        while subseq_last_idx <= stop_position:
+            indices.append((subseq_first_idx, subseq_mid_idx, subseq_last_idx))
+            subseq_first_idx += step_size
+            subseq_mid_idx += step_size
+            subseq_last_idx += step_size
+
+        return indices
+
+
+def transform_ts_data_into_features_and_target(
+    ts_data: pd.DataFrame,
+    input_seq_len: int,
+    step_size: int
+) -> Tuple[pd.DataFrame, pd.Series]:
+    """
+    Slices and transposes data from time-series format into a (features, target)
+    format that we can use to train Supervised ML models
+    """
+    assert set(ts_data.columns) == {'pickup_hour', 'rides', 'pickup_location_id'}
+
+    location_ids = ts_data['pickup_location_id'].unique()
+    features = pd.DataFrame()
+    targets = pd.DataFrame()
+    
+    for location_id in tqdm(location_ids):
+        
+        # keep only ts data for this `location_id`
+        ts_data_one_location = ts_data.loc[
+            ts_data.pickup_location_id == location_id, 
+            ['pickup_hour', 'rides']
+        ].sort_values(by=['pickup_hour'])
+
+        # pre-compute cutoff indices to split dataframe rows
+        indices = get_cutoff_indices_features_and_target(
+            ts_data_one_location,
+            input_seq_len,
+            step_size
+        )
+
+        # slice and transpose data into numpy arrays for features and targets
+        n_examples = len(indices)
+        x = np.ndarray(shape=(n_examples, input_seq_len), dtype=np.float32)
+        y = np.ndarray(shape=(n_examples), dtype=np.float32)
+        pickup_hours = []
+        for i, idx in enumerate(indices):
+            x[i, :] = ts_data_one_location.iloc[idx[0]:idx[1]]['rides'].values
+            y[i] = ts_data_one_location.iloc[idx[1]:idx[2]]['rides'].values[0]
+            pickup_hours.append(ts_data_one_location.iloc[idx[1]]['pickup_hour'])
+
+        # numpy -> pandas
+        features_one_location = pd.DataFrame(
+            x,
+            columns=[f'rides_previous_{i+1}_hour' for i in reversed(range(input_seq_len))]
+        )
+        features_one_location['pickup_hour'] = pickup_hours
+        features_one_location['pickup_location_id'] = location_id
+
+        # numpy -> pandas
+        targets_one_location = pd.DataFrame(y, columns=[f'target_rides_next_hour'])
+
+        # concatenate results
+        features = pd.concat([features, features_one_location])
+        targets = pd.concat([targets, targets_one_location])
+
+    features.reset_index(inplace=True, drop=True)
+    targets.reset_index(inplace=True, drop=True)
+
+    return features, targets['target_rides_next_hour']
